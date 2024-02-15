6fc1de4b6f91b8858d3cc8344ef8d97f99e8ad29
dxqwww <minikkat1337@gmail.com>
2024-02-15 13:22:37 +0300

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         osu!skillpicker
+
+// unrestrict me please...
+// @namespace    https://osu.ppy.sh/users/9003579
+// @version      1.0.0
+// @description  script for osu! which provides the ability to mark, search and view skellsets for all beatmaps.
+// @author       dxqwww & ReeStick
+// @match        http://osu.ppy.sh/*
+// @match        https://osu.ppy.sh/*
+
+// @run-at       document-idle
+
+// @noframes
+// @grant    GM_setValue
+// @grant    GM_getValue
+// @grant    GM_listValues
+// ==/UserScript==
+
+/*global $ */
+
+(() => {
+
+});
