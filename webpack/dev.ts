6ecf961b1313f0f0b5893592bc46639465c2d66f
dxqwww <minikkat1337@gmail.com>
2024-03-22 10:58:53 +0300

    feature: added compiled userscript

--- webpack/dev.ts
+++ webpack/dev.ts
@@ -10,7 +10,7 @@ export default merge(base, {
     },
     output: {
         path: path.resolve(".", "userscripts"),
-        filename: "index.dev.user.js",
+        filename: "skillpicker.dev.user.js",
     },
     devtool: 'eval-source-map',
     watch: true,
